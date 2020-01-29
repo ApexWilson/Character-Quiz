d297f70bbb36a248a27f5bcb20ded581144026c1
ApexWilson <wongwilson112@gmail.com>
2020-01-29 02:14:53 +0000

    fix input class

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // Write your click handler and conditionals here
+$("button").click(function(){
+    var points=0;
+    let class = $(".class").val();
+    function questionOne(){
+        if(class==="mage"){
+            point+4;
+        }
+    }
+    function questionTwo(){
+        if(){
+        
+        }
+    }
+    function questionThree(){
+        if(){
+        
+        }
+    }
+    function questionFour(){
+        if(){
+        
+        }
+    }
+});
